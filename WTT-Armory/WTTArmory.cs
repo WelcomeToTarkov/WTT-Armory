14898a171d11d6fd0feb7d2aa20f9d020b83b197
GrooveypenguinX <jakechase2110@gmail.comX>
2026-01-25 21:16:20 -0500

    version bump for commonlib dep

--- WTT-Armory/WTTArmory.cs
+++ WTT-Armory/WTTArmory.cs
@@ -21,7 +21,7 @@ public record ModMetadata : AbstractModMetadata
     public override List<string>? Incompatibilities { get; init; }
     public override Dictionary<string, Range>? ModDependencies { get; init; } = new()
     {
-        { "com.wtt.commonlib", new Range("~2.0.12") }
+        { "com.wtt.commonlib", new Range("~2.0.13") }
     };
     public override string? Url { get; init; }
     public override bool? IsBundleMod { get; init; } = true;
